a61dba3ea9b21e031ab5c383dd59e01b91eaaee8
shohan-ch <shohan889@yahoo.com>
2024-06-09 22:44:22 +0600

    feat (nav): nav bar layout added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,15 @@
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>Product page</div>;
+  return (
+    <div>
+      Product page
+      <h2 className="bg-primary">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt,
+        voluptatibus!
+      </h2>
+    </div>
+  );
 };
 
 export default page;
---
+++ src/icons/IconSvg.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+const icons: any = {
+  logo: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={34}
+      height={34}
+      color="#02b290"
+      fill="none"
+    >
+      <path
+        d="M3.06164 15.1933L3.42688 13.1219C3.85856 10.6736 4.0744 9.44952 4.92914 8.72476C5.78389 8 7.01171 8 9.46734 8H14.5327C16.9883 8 18.2161 8 19.0709 8.72476C19.9256 9.44952 20.1414 10.6736 20.5731 13.1219L20.9384 15.1933C21.5357 18.5811 21.8344 20.275 20.9147 21.3875C19.995 22.5 18.2959 22.5 14.8979 22.5H9.1021C5.70406 22.5 4.00504 22.5 3.08533 21.3875C2.16562 20.275 2.4643 18.5811 3.06164 15.1933Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M7.5 8L7.66782 5.98618C7.85558 3.73306 9.73907 2 12 2C14.2609 2 16.1444 3.73306 16.3322 5.98618L16.5 8"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M15 11C14.87 12.4131 13.5657 13.5 12 13.5C10.4343 13.5 9.13002 12.4131 9 11"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  ),
+
+  cart: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={32}
+      height={32}
+      color={"#4a4a4a"}
+      fill={"none"}
+    >
+      <path
+        d="M4.34085 16.8781L3.17786 9.93557C2.98869 8.8063 2.89411 8.24167 3.18537 7.87083C3.47662 7.5 4.01468 7.5 5.09079 7.5H18.9092C19.9853 7.5 20.5234 7.5 20.8146 7.87083C21.1059 8.24167 21.0113 8.8063 20.8221 9.93557L19.6591 16.8781C19.249 19.3264 19.044 20.5505 18.2319 21.2752C17.4199 22 16.2534 22 13.9204 22H10.0796C7.74664 22 6.58014 22 5.76809 21.2752C4.95603 20.5505 4.75097 19.3264 4.34085 16.8781Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M7 7.5V7C7 4.23858 9.23858 2 12 2C14.7614 2 17 4.23858 17 7V7.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+      <path d="M4.5 17.5H19.5" stroke="currentColor" strokeWidth="1.5" />
+    </svg>
+  ),
+  userAvatar: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={32}
+      height={32}
+      color={"#4a4a4a"}
+      fill={"none"}
+    >
+      <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="1.5" />
+      <path
+        d="M7.5 17C9.8317 14.5578 14.1432 14.4428 16.5 17M14.4951 9.5C14.4951 10.8807 13.3742 12 11.9915 12C10.6089 12 9.48797 10.8807 9.48797 9.5C9.48797 8.11929 10.6089 7 11.9915 7C13.3742 7 14.4951 8.11929 14.4951 9.5Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  ),
+};
+
+type Props = {
+  name: string;
+  width?: string;
+  height?: string;
+};
+
+const IconSvg = (props: Props) => {
+  const { name, width, height } = props;
+  return <>{icons[name]}</>;
+};
+
+export default IconSvg;
