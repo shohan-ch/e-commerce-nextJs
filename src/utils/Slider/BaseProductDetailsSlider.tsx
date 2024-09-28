755f380bbfc3f0cefad22c361dd1444068cc3053
shohan-ch <shohan889@yahoo.com>
2024-09-29 04:03:52 +0600

    feat: scroll to left and right added in slider

--- src/utils/Slider/BaseProductDetailsSlider.tsx
+++ src/utils/Slider/BaseProductDetailsSlider.tsx
@@ -1,6 +1,6 @@
 import IconSvg from "@/icons/IconSvg";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 type Props = {
   title: string;
@@ -9,20 +9,34 @@ type Props = {
 
 const BaseProductDetailsSlider = (props: Props) => {
   const { data, title } = props;
+  const productRef = useRef<HTMLDivElement | any>(null);
+  const productDivRef = useRef<HTMLDivElement | any>(null);
 
-  const handleScroll = (type: string) => () => {
-    alert(type);
+  const handleScrollToX = (direction: "right" | "left") => () => {
+    // console.log(productDivRef.current?.offsetWidth);
+    // console.log(productRef.current?.offsetWidth);
+
+    const productWidth = productRef.current.offsetWidth;
+    if (direction == "right") {
+      productDivRef.current.scrollLeft += productWidth;
+    } else {
+      productDivRef.current.scrollLeft -= productWidth;
+    }
   };
 
   return (
     <>
       <div className="mt-12 relative">
         <h3 className="font-bold text-xl mb-5">{title}</h3>
-        <div className=" flex justify-evenly gap-4 overflow-x-auto h-[380px] scroll-smooth">
+        <div
+          ref={productDivRef}
+          className=" flex justify-evenly gap-4 overflow-x-auto h-[380px] scroll-smooth"
+        >
           {data &&
             data.map((product: any, index: number) => {
               return (
                 <div
+                  ref={productRef}
                   key={index}
                   className="w-[250px] flex-none  border rounded-lg shadow-sm p-4 cursor-pointer group hover:shadow-md transition-all duration-200"
                 >
@@ -57,7 +71,7 @@ const BaseProductDetailsSlider = (props: Props) => {
         {/* Next Prev Buttons */}
         <div
           className={`cursor-pointer absolute top-[55%] hover:border-primary transition-colors duration-300 -left-4 border shadow-md transform -translate-y-1/2  bg-white size-10 rounded-full`}
-          onClick={handleScroll("decrement")}
+          onClick={handleScrollToX("left")}
         >
           <div className="h-full flex justify-center items-center ">
             <IconSvg name="prev" />
@@ -66,7 +80,7 @@ const BaseProductDetailsSlider = (props: Props) => {
 
         <div
           className={`cursor-pointer absolute top-[55%]  -right-4 border hover:border-primary transition-colors duration-300 shadow-md transform -translate-y-1/2 bg-white size-10 rounded-full`}
-          onClick={handleScroll("increment")}
+          onClick={handleScrollToX("right")}
         >
           <div className="h-full flex justify-center items-center">
             <IconSvg name="next" />
