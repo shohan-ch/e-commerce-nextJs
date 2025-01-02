2dff90a75efd5abec96769dfaf081591c2f1a1a2
shohan-ch <shohan889@yahoo.com>
2025-01-02 19:24:03 +0600

    refactor: base Radio with icons

--- src/icons/IconSvg.tsx
+++ src/icons/IconSvg.tsx
@@ -1,5 +1,30 @@
 import React from "react";
 const icons: any = {
+  briefCase: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="gray"
+      className="bi bi-briefcase-fill"
+      viewBox="0 0 16 16"
+    >
+      <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v1.384l7.614 2.03a1.5 1.5 0 0 0 .772 0L16 5.884V4.5A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5" />
+      <path d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85z" />
+    </svg>
+  ),
+  home: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      fill="gray"
+      className="bi bi-house-door-fill"
+      viewBox="0 0 16 16"
+    >
+      <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
+    </svg>
+  ),
   delete: (
     <svg
       xmlns="http://www.w3.org/2000/svg"
